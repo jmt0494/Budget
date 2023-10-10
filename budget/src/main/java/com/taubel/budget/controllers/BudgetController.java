994ee55252bcdf56e6d9013082eb075e7d38446f
Joshua Taubel <josh.taubel@gmail.com>
2023-10-10 16:11:51 -0500

    PreAuth added to Budget controller

--- budget/src/main/java/com/taubel/budget/controllers/BudgetController.java
+++ budget/src/main/java/com/taubel/budget/controllers/BudgetController.java
@@ -1,6 +1,7 @@
 package com.taubel.budget.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,23 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.taubel.budget.entities.Budget;
 
 @RestController
-@RequestMapping("/{username}/budget")
+@RequestMapping("/{username}/budget/{month}/{year}")
+@PreAuthorize("#username == authentication.principal.username")
 public class BudgetController {
     
-    //All methods with alter the budget as well as any dependent data (category, LineItem, Transaction)
-
-    @GetMapping("/{month}")
-    ResponseEntity<Budget> getBudget(@PathVariable("username") String usename, @PathVariable("month") String month) {
+    //All methods will alter the budget as well as any dependent data (category, LineItem, Transaction)
+    
+    @GetMapping()
+    ResponseEntity<Budget> getBudget(@PathVariable("username") String username, @PathVariable("month") String month, @PathVariable("year") String year) {
         return ResponseEntity.ok(new Budget()); //TODO retrieve from database
     }
     
-    @PostMapping("/{month}")
-    ResponseEntity<Budget> createBudget(@PathVariable("username") String usename, @PathVariable("month") String month) {
+    @PostMapping()
+    ResponseEntity<Budget> createBudget(@PathVariable("username") String username, @PathVariable("month") String month, @PathVariable("year") String year) {
         return ResponseEntity.ok(new Budget()); //TODO
     }
 
-    @DeleteMapping("/{month}")
-    ResponseEntity<Budget> deleteBudget(@PathVariable("username") String usename, @PathVariable("month") String month) {
+    @DeleteMapping()
+    ResponseEntity<Budget> deleteBudget(@PathVariable("username") String username, @PathVariable("month") String month, @PathVariable("year") String year) {
         return ResponseEntity.ok(null); //TODO
     }
 
