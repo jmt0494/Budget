d953992e2001a29feae492f2bbc4939b0b6fcbd4
Joshua Taubel <josh.taubel@gmail.com>
2023-12-08 14:21:30 -0600

    Router set up

--- budget-frontend/src/app/app.module.ts
+++ budget-frontend/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AppRoutingModule } from './modules/routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
