28786fef7d66150380b9979cf40193ae4a541c71
Joshua Taubel <josh.taubel@gmail.com>
2023-12-19 11:20:27 -0600

    cleaned up imports

--- FerengiFin/src/app/app.module.ts
+++ FerengiFin/src/app/app.module.ts
@@ -1,19 +1,15 @@
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
 
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { AuthInterceptor } from './core/interceptors/auth-interceptor';
-import { LoginModule } from './features/login/feature/login.module';
-import { BudgetModule } from './features/budget/feature/budget-shell/budget.module';
 import { CoreModule } from './core/core.module';
-import { NavComponent } from './core/nav/nav.component';
+import { AuthInterceptor } from './core/interceptors/auth-interceptor';
+import { BudgetModule } from './features/budget/feature/budget-shell/budget.module';
+import { LoginModule } from './features/login/feature/login.module';
 
 @NgModule({
   declarations: [
